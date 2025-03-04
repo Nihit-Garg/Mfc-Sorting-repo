a6075c0fb7c7b09a6e28ae4ffe9ef925a4753d49
NIHIT GARG <nihitgarg@NIHITs-MacBook.local>
2025-03-04 12:41:10 +0530

    initial commit

---
+++ Bubblesort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Enter the number of elements\n";
+    cin >>n;
+
+    int arr[n];
+    cout<<"Enter "<<n<<" Elements\n";
+    for(int i=0;i<n;i++){ 
+        cin >>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j+1]<arr[j]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<10;i++){
+        cout<< arr[i]<<" ";
+    }
+    return 0;
+}
